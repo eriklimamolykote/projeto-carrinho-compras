4357d245be23c3961d565c85944654af8a09629e
Erik Augusto Lima Evangelista <eriklim@hotmail.com>
2022-01-17 10:32:00 -0300

    primeiro-commit-projeto-carrinho-compras-17-01-2022

---
+++ app.js
@@ -0,0 +1,130 @@
+// SELECIONA ELEMENTOS
+const productsEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items");
+const subtotalEl = document.querySelector(".subtotal");
+const totalItemsInCartEl = document.querySelector(".total-items-in-cart");
+
+// RENDERIZA PRODUTOS
+function renderProdcuts() {
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+            <div class="item">
+                <div class="item-container">
+                    <div class="item-img">
+                        <img src="${product.imgSrc}" alt="${product.name}">
+                    </div>
+                    <div class="desc">
+                        <h2>${product.name}</h2>
+                        <h2><small>R$</small>${product.price}</h2>
+                        <p>
+                            ${product.description}
+                        </p>
+                    </div>
+                    <div class="add-to-wishlist">
+                        <img src="./icons/heart.png" alt="add to wish list">
+                    </div>
+                    <div class="add-to-cart" onclick="addToCart(${product.id})">
+                        <img src="./icons/bag-plus.png" alt="add to cart">
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+}
+renderProdcuts();
+
+// array do carrinho
+let cart = JSON.parse(localStorage.getItem("CART")) || [];
+updateCart();
+
+// ADICIONA AO CARRINHO
+function addToCart(id) {
+  // verifica se o produto já está no carrinho
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else {
+    const item = products.find((product) => product.id === id);
+
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    });
+  }
+
+  updateCart();
+}
+
+// atualiza o carrinho
+function updateCart() {
+  renderCartItems();
+  renderSubtotal();
+
+  // salva o carrinho para o armazenamento local
+  localStorage.setItem("CART", JSON.stringify(cart));
+}
+
+// calcula e renderiza o subtotal
+function renderSubtotal() {
+  let totalPrice = 0,
+    totalItems = 0;
+
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
+    totalItems += item.numberOfUnits;
+  });
+
+  subtotalEl.innerHTML = `Subtotal (${totalItems} items): $${totalPrice.toFixed(2)}`;
+  totalItemsInCartEl.innerHTML = totalItems;
+}
+
+// renderiza os ítens do carrinho
+function renderCartItems() {
+  cartItemsEl.innerHTML = ""; // limpa o elemento do carrinho
+  cart.forEach((item) => {
+    cartItemsEl.innerHTML += `
+        <div class="cart-item">
+            <div class="item-info" onclick="removeItemFromCart(${item.id})">
+                <img src="${item.imgSrc}" alt="${item.name}">
+                <h4>${item.name}</h4>
+            </div>
+            <div class="unit-price">
+                <small>R$</small>${item.price}
+            </div>
+            <div class="units">
+                <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+                <div class="number">${item.numberOfUnits}</div>
+                <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>           
+            </div>
+        </div>
+      `;
+  });
+}
+
+// remove ítem do carrinho
+function removeItemFromCart(id) {
+  cart = cart.filter((item) => item.id !== id);
+
+  updateCart();
+}
+
+// altera o número de unidades para um determinado ítem
+function changeNumberOfUnits(action, id) {
+  cart = cart.map((item) => {
+    let numberOfUnits = item.numberOfUnits;
+
+    if (item.id === id) {
+      if (action === "minus" && numberOfUnits > 1) {
+        numberOfUnits--;
+      } else if (action === "plus" && numberOfUnits < item.instock) {
+        numberOfUnits++;
+      }
+    }
+
+    return {
+      ...item,
+      numberOfUnits,
+    };
+  });
+
+  updateCart();
+}
---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Camiseta 1",
+    price: 165.51,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "Camiseta 2",
+    price: 138.32,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "Camiseta 3",
+    price: 110.64,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "Camiseta 4",
+    price: 143.85,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "Camiseta 5",
+    price: 165.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "Camiseta 6",
+    price: 221.34,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/t6.png",
+  },
+];
